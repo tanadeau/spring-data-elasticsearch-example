a68620151a860d6aea3d2be792a3d2a449154eb9
Trent Nadeau <Trent.Nadeau@jhuapl.edu>
2016-12-29 13:36:26 -0500

    Use @Field annotation properly

--- src/main/kotlin/com/example/model/Post.kt
+++ src/main/kotlin/com/example/model/Post.kt
@@ -3,6 +3,7 @@ package com.example.model
 import org.springframework.data.annotation.Id
 import org.springframework.data.elasticsearch.annotations.Document
 import org.springframework.data.elasticsearch.annotations.Field
+import org.springframework.data.elasticsearch.annotations.FieldIndex
 import org.springframework.data.elasticsearch.annotations.FieldType
 
 @Document(indexName = "example", type = "post")
@@ -14,6 +15,8 @@ data class Post(
 )
 
 data class Tag(
+        @Field(type = FieldType.String, index = FieldIndex.not_analyzed)
         val id: String,
+        @Field(type = FieldType.String)
         val name: String
 )
