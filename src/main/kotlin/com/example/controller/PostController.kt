91244ca2a021007f4c0702016145d33e9492b331
Trent Nadeau <Trent.Nadeau@jhuapl.edu>
2017-01-03 15:18:41 -0500

    Add more controller info in annotations

--- src/main/kotlin/com/example/controller/PostController.kt
+++ src/main/kotlin/com/example/controller/PostController.kt
@@ -5,6 +5,7 @@ import com.example.model.Tag
 import com.example.service.PostService
 import mu.KLogging
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.concurrent.atomic.AtomicInteger
@@ -14,21 +15,24 @@ import java.util.concurrent.atomic.AtomicInteger
 class PostController(val postService: PostService) {
     companion object : KLogging()
 
-    @GetMapping
+    @GetMapping(produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
     fun getAll(): List<Post> {
         return postService.findAll().toList()
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}", produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
     fun getById(@PathVariable id: String): ResponseEntity<Post> {
         val found = postService.findOne(id)
         return if (found == null) { ResponseEntity.notFound().build() } else { ResponseEntity.ok(found) }
     }
 
-    @PostMapping
+    @PostMapping(
+            consumes = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE),
+            produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody newPost: Post) {
+    fun create(@RequestBody newPost: Post): Post {
         val saved = postService.save(newPost)
         logger.info { "Saved new post with ID ${saved.id}" }
+        return saved
     }
 }
