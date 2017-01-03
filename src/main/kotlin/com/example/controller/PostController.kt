2a751c27003760dc16421cbf25714c1e661ae7d3
Trent Nadeau <Trent.Nadeau@jhuapl.edu>
2017-01-03 15:10:15 -0500

    Have create in controller accept input

--- src/main/kotlin/com/example/controller/PostController.kt
+++ src/main/kotlin/com/example/controller/PostController.kt
@@ -3,16 +3,17 @@ package com.example.controller
 import com.example.model.Post
 import com.example.model.Tag
 import com.example.service.PostService
+import mu.KLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.concurrent.atomic.AtomicInteger
 
-val counter = AtomicInteger(0)
-
 @RestController
 @RequestMapping("/post")
 class PostController(val postService: PostService) {
+    companion object : KLogging()
+
     @GetMapping
     fun getAll(): List<Post> {
         return postService.findAll().toList()
@@ -26,11 +27,8 @@ class PostController(val postService: PostService) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create() {
-        val newPost = Post(
-                null,
-                listOf(Tag("tag1", "value${counter.andIncrement}"), Tag("tag2", "value${counter.andIncrement}")))
-
-        postService.save(newPost)
+    fun create(@RequestBody newPost: Post) {
+        val saved = postService.save(newPost)
+        logger.info { "Saved new post with ID ${saved.id}" }
     }
 }
