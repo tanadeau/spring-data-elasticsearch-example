38fe83420e7fe9a66a860dee6028692ee59df7b7
James Stapleton <github@tasermonkeys.com>
2017-01-08 10:55:21 -0500

    Should be internal, as nothing outside the file should see this

--- src/main/kotlin/com/example/stomp/StompEvents.kt
+++ src/main/kotlin/com/example/stomp/StompEvents.kt
@@ -15,7 +15,7 @@ import org.springframework.web.socket.messaging.SessionConnectEvent
 import org.springframework.web.socket.messaging.SessionDisconnectEvent
 import java.util.concurrent.ConcurrentHashMap
 
-typealias SessionId = String
+internal typealias SessionId = String
 // required to be thread safe
 internal val sessionRepo = ConcurrentHashMap<SessionId, KeycloakAuthenticationToken>()
 
