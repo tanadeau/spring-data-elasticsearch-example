56fb02e3ce4137dc516014e8a330d0650d0fab44
James Stapleton <github@tasermonkeys.com>
2017-01-08 10:23:27 -0500

    change var to val

--- src/main/kotlin/com/example/service/StompLiveEventsService.kt
+++ src/main/kotlin/com/example/service/StompLiveEventsService.kt
@@ -10,7 +10,7 @@ interface StompLiveEventsService {
 }
 
 @Service
-class StompLiveEventsServiceImpl(private var messagingTemplate: MessageSendingOperations<String>) : StompLiveEventsService {
+class StompLiveEventsServiceImpl(private val messagingTemplate: MessageSendingOperations<String>) : StompLiveEventsService {
     override fun save(authorizable: Authorizable) {
         messagingTemplate.convertAndSend("/topic/activity", authorizable, { msg ->
             MessageBuilder.fromMessage(msg).setHeader(Authorizable::visibilities.name, authorizable.visibilities).build()
